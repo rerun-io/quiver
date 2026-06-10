6abbe3468ac413df882eadff184632a6a8833503
Emil Ernerfeldt <emil.ernerfeldt@gmail.com>
2026-06-10 20:50:24 +0200

    Add support for arrow 59 (#12)

--- scripts/test_arrow_versions.sh
+++ scripts/test_arrow_versions.sh
@@ -9,7 +9,7 @@ cd "$(dirname "$0")/.."
 
 # The floor of every supported arrow major.
 # The locked (newest) version is tested last.
-ARROW_VERSIONS=("57.0.0" "58.0.0")
+ARROW_VERSIONS=("57.0.0" "58.0.0" "59.0.0")
 
 if [ ! -f Cargo.lock ]; then
     # E.g. on CI: Cargo.lock is not committed.
